3908353f67c548465a90b4d94fbd079971b8721b
Vekrotis <156781624+Vekrotis@users.noreply.github.com>
2024-06-16 22:41:27 +0200

    Remake V1.4

--- README.md
+++
@@ -1,1 +0,0 @@
-matynkaaa.x first website
---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+});
